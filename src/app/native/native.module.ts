17c0397a8c27c99b15424384c5c93effe2e50790
Yiyao Zhang <me@yiyao.ca>
2021-03-05 21:35:13 -0500

    remove unused file

--- src/app/native/native.module.ts
+++ src/app/native/native.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SecurestorageComponent } from './securestorage/securestorage.component';
 import { AppversionComponent } from './appversion/appversion.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [SecurestorageComponent, AppversionComponent],
+  declarations: [AppversionComponent],
   imports: [
     CommonModule, IonicModule, FormsModule
   ],
-  exports: [SecurestorageComponent, AppversionComponent]
+  exports: [AppversionComponent]
 })
 export class NativeModule { }
